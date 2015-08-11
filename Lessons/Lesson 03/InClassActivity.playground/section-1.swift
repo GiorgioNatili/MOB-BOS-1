8cde7ea34f7e5432a32b5d7ce8bdfcde79c1a6ae
Dmitry Shamis <dshamis@hubspot.com>
2015-08-10 21:01:46 -0400

    Complete first 3 exercises

--- Lessons/Lesson 03/InClassActivity.playground/section-1.swift
+++ Lessons/Lesson 03/InClassActivity.playground/section-1.swift
@@ -3,11 +3,20 @@
 import Foundation
 
 // TODO: Create two variables, name and age. Name is a string, age is an integer.
+var name = "Dmitry"
+var age = 28
 
 // TODO: Print "Hello {whatever the value of name is}, you are {whatever the value of age is} years old!"
-
+println("Hello \(name), you are \(age) years old!")
 
 // TODO: Print “You can drink” below the above text if the user is above 21. If they are above 18, print “you can vote”. If they are above 16, print “You can drive”
+if age > 21 {
+    println("You can drink")
+} else if age > 18 {
+    println("You can vote")
+} else if age > 16 {
+    println("You can drive")
+}
 
 // TODO: Print “you can drive” if the user is above 16 but below 18. It should print “You can drive and vote” if the user is above 18 but below 21. If the user is above 21, it should print “you can drive, vote and drink (but not at the same time!”.
 
