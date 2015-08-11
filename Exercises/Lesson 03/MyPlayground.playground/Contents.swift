9478d65944167970c427823b98fdafc189c911e3
MaxBlaushild <blaushmild@gmail.com>
2015-08-10 21:21:59 -0400

    refactors last question to use arc4random to not clock out after 3 iterations

--- Exercises/Lesson 03/MyPlayground.playground/Contents.swift
+++ Exercises/Lesson 03/MyPlayground.playground/Contents.swift
@@ -26,10 +26,10 @@ if number % 2 == 0 {
 
 println("Hello \(name), your name is \(count(name)) characters long!")
 
-var sum:Int32 = 0
+var sum:UInt32 = 0
 
 var rangeTwo = 1...100
 for i in range {
-    sum = (sum + rand())
+    sum += arc4random_uniform(100)
 }
 
