b27a88174254034433d48576913f03b8482ae6b3
Dmitry Shamis <dshamis@hubspot.com>
2015-08-10 21:41:22 -0400

    Complete exercise 6

--- Lessons/Lesson 03/InClassActivity.playground/section-1.swift
+++ Lessons/Lesson 03/InClassActivity.playground/section-1.swift
@@ -5,6 +5,7 @@ import Foundation
 // TODO: Create two variables, name and age. Name is a string, age is an integer.
 var name = "Dmitry"
 var age = 28
+var zzz = 777
 
 // TODO: Print "Hello {whatever the value of name is}, you are {whatever the value of age is} years old!"
 println("Hello \(name), you are \(age) years old!")
@@ -33,14 +34,42 @@ for var i = 1; i <= 50; i++ {
 }
 
 // TODO: Create a constant called number
-
+let number = 17
 
 // TODO: Print whether the above number is even
+if number % 2 == 0 {
+    println("\(number) is even")
+} else {
+    println("\(number) is odd")
+}
 
 // TODO: Print out "Hello {whatever the value of name is}, your name is {however long the string name is} characters long!. Use countElements()
+println("Hello \(name), your name is \(count(name)) characters long!")
 
 // TODO: Print the sum of one hundred random numbers. Use rand() to generate random numbers.
+var sum:Int32 = 0
+
+for var j = 0; j < 100; j++ {
+    sum += (rand() % 100)
+}
+
+println(sum)
 
 // Bonus TO DO: Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+func fizzBuzz(num:Int) {
+    for var x=1;x<=num;x++ {
+        if x % 5 == 0 && x % 3 == 0 {
+            println("FizzBuzz")
+        } else if x % 5 == 0 {
+            println("Buzz")
+        } else if x % 3 == 0 {
+            println("Fizz")
+        } else {
+            println(num)
+        }
+    }
+}
+
+fizzBuzz(100)
 
 // BONUS TODO: The first fibonacci number is 0, the second is 1, the third is 1, the fourth is two, the fifth is 3, the sixth is 5, etc. The Xth fibonacci number is the sum of the X-1th fibonacci number and the X-2th fibonacci number. Print the 37th fibonacci number below
