ddc13acd34f74f746c7bd5b856b7468e664ca510
MaxBlaushild <blaushmild@gmail.com>
2015-08-10 21:09:08 -0400

    Finishes the assessments minus the bonuses. Last question quickly exceeds the limits of a 32 bit signature tho

---
+++ Exercises/Lesson 03/MyPlayground.playground/Contents.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+var name = "Max Blaushild"
+
+var age = 25
+
+println("Hello \(name), you are \(age) years old")
+
+if age > 16 {
+    println("You can drive")
+} else {
+    println("sucks to suck")
+}
+
+var range = 1...50
+
+for i in range {
+    println("\(i * 7 - 1)")
+}
+
+let number = 12
+
+if number % 2 == 0 {
+    println("even")
+}
+
+println("Hello \(name), your name is \(count(name)) characters long!")
+
+var sum:Int32 = 0
+
+var rangeTwo = 1...100
+for i in range {
+    sum = (sum + rand())
+}
+
