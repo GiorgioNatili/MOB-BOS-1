cd1af7155796662807d818b73141aeb3b9f0c0b3
Dmitry Shamis <dshamis@hubspot.com>
2015-08-10 21:43:43 -0400

    Begin work on Fibonacci's numbers

--- Lessons/Lesson 03/InClassActivity.playground/section-1.swift
+++ Lessons/Lesson 03/InClassActivity.playground/section-1.swift
@@ -73,3 +73,7 @@ func fizzBuzz(num:Int) {
 fizzBuzz(100)
 
 // BONUS TODO: The first fibonacci number is 0, the second is 1, the third is 1, the fourth is two, the fifth is 3, the sixth is 5, etc. The Xth fibonacci number is the sum of the X-1th fibonacci number and the X-2th fibonacci number. Print the 37th fibonacci number below
+func fibonacci(num:Int) {
+    
+}
+
