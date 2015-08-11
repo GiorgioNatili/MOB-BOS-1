55132db1f37376dbf831dc0d956153bf9c10ddaa
MaxBlaushild <blaushmild@gmail.com>
2015-08-10 21:41:27 -0400

    does bonuses too

--- Exercises/Lesson 03/MyPlayground.playground/Contents.swift
+++ Exercises/Lesson 03/MyPlayground.playground/Contents.swift
@@ -28,8 +28,47 @@ println("Hello \(name), your name is \(count(name)) characters long!")
 
 var sum:UInt32 = 0
 
-var rangeTwo = 1...100
-for i in range {
-    sum += arc4random_uniform(100)
+func getRandom100() -> UInt32 {
+    var rangeTwo = 1...100
+    for i in rangeTwo {
+        sum += arc4random_uniform(100)
+    }
+    return sum
 }
+
+println(getRandom100())
+
+func fillBuzz(){
+    var range = 1...100
+    for i in range {
+        if i % 15 == 0 {
+            println("fizzbuzz")
+        } else if i % 5 == 0 {
+            println("buzz")
+        } else if i % 3 == 0 {
+            println("fizz")
+        } else {
+            println("\(i)")
+        }
+    }
+}
+
+fillBuzz()
+
+func fibonnaci() -> Int {
+    var count = 1
+    var x = 0
+    var y = 1
+    var temp = 0
+    
+    while count < 38 {
+        temp = y
+        y = x + temp
+        x = temp
+        count++
+    }
+    return temp
+}
+
+fibonnaci()
 
