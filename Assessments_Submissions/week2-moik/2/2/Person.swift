60b40bf1405c3265f2e96899c27ca46c587d4713
Yofred Moik <ytmoik@gmail.com>
2015-08-17 20:43:02 -0400

    add private

--- Assessments_Submissions/week2-moik/2/2/Person.swift
+++ Assessments_Submissions/week2-moik/2/2/Person.swift
@@ -10,13 +10,34 @@ import Foundation
 
 class Person {
 
-    var name : String
-    var age : Int?
+    private var name : String
+    private var age : Int?
     
     init (name:String, age:Int?) {
         self.name = name
         self.age = age
     }
+    
+    var realName: String {
+        get {
+            return name
+        }
+        
+        set {
+            name = newValue
+        }
+    }
+    
+    var realAge: Int? {
+        get {
+            return age
+        }
+        
+        set {
+            age = newValue
+        }
+    }
+
     
     func checkAge(age:Int) -> String {
         
