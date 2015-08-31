48d759ef454d54e359b62859e40b33bc724ad909
Bo-Huei Lin <bohueilin@hotmail.com>
2015-08-31 17:53:45 -0400

    week4-lin

---
+++ week4-lin/week4-lin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week4-lin
+//
+//  Created by Bo-Huei Lin on 8/31/15.
+//  Copyright (c) 2015 Bo-Huei Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
